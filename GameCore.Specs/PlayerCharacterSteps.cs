d68be2b622dc4d5be147945a672956676f40568e
Hnin Wuttyi <wuttyi@solidplm.com>
2020-02-20 11:47:25 +0630

    Test for Damage 0.

--- GameCore.Specs/PlayerCharacterSteps.cs
+++ GameCore.Specs/PlayerCharacterSteps.cs
@@ -1,27 +1,30 @@
 ﻿using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace GameCore.Specs
 {
     [Binding]
     public class PlayerCharacterSteps
     {
+        private PlayerCharacter _player;
+
         [Given(@"I'm a new player")]
         public void GivenImANewPlayer()
         {
-            ScenarioContext.Current.Pending();
+            _player = new PlayerCharacter();
         }
 
         [When(@"I take 0 damage")]
         public void WhenITake0Damage()
         {
-            ScenarioContext.Current.Pending();
+            _player.Hit(0);
         }
 
         [Then(@"My health should now be 100")]
         public void ThenMyHealthShouldNowBe100()
         {
-            ScenarioContext.Current.Pending();
+            Assert.Equal(100, _player.Health);
         }
     }
 }
