dc8a8d339acac90323e1228068f9d07672e8093a
Hnin Wuttyi <wuttyi@solidplm.com>
2020-02-21 13:08:15 +0630

    Get multi value from Step data table.

--- GameCore.Specs/PlayerCharacterSteps.cs
+++ GameCore.Specs/PlayerCharacterSteps.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using TechTalk.SpecFlow;
 using Xunit;
 using TechTalk.SpecFlow.Assist;
+using System.Collections.Generic;
 
 namespace GameCore.Specs
 {
@@ -71,7 +72,47 @@ namespace GameCore.Specs
         public void WhenCastAHealingSpell()
         {
             _player.CastHealingSpell();
+        }
+
+        [Given(@"I have the following magical items")]
+        public void GivenIHaveTheFollowingMagicalItems(Table table)
+        {
+            //foreach (var row in table.Rows)
+            //{
+            //    var name = row["item"];
+            //    var value = row["value"];
+            //    var power = row["power"];
+
+            //    _player.MagicalItems.Add(new MagicalItem
+            //    {
+            //        Name = name,
+            //        Value = int.Parse(value),
+            //        Power = int.Parse(power)
+            //    });
+            //}
+
+            //IEnumerable<MagicalItem> items = table.CreateSet<MagicalItem>();
+
+            //_player.MagicalItems.AddRange(items);
+
+            IEnumerable<dynamic> items = table.CreateDynamicSet();
+            foreach (var magicalItem in items)
+            {
+                _player.MagicalItems.Add(new MagicalItem
+                {
+                    Name = magicalItem.name,
+                    Value = magicalItem.value,
+                    Power = magicalItem.power
+                });
+            }
 
         }
+
+        [Then(@"My total magical power should be (.*)")]
+        public void ThenMyTotalMagicalPowerShouldBe(int expectedPower)
+        {
+            Assert.Equal(expectedPower, _player.MagicalPower);
+        }
+
     }
 }
